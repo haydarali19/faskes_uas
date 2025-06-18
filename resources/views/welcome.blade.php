d097133f8bb0619f42abf86ce85a7cdc56f6e108
Aurora Zalfa Hartono <aurorazalhartono@gmail.com>
2025-06-18 15:06:33 +0700

    ada yg ketinggalan 1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -657,7 +657,7 @@ Kami memahami bahwa hewan peliharaan adalah bagian dari keluarga. Oleh karena it
       <!-- Section Title -->
       <div class="container section-title" data-aos="fade-up">
         <h2>Gallery</h2>
-        <p>Necessitatibus eius consequatur ex aliquid fuga eum quidem sint consectetur velit</p>
+        <p>Lihat langsung bagaimana kami merawat anjing dan kucing dengan penuh dedikasi. Setiap momen di sini adalah bukti komitmen kami untuk memberikan perawatan terbaik bagi hewan peliharaan Anda.</p>
       </div><!-- End Section Title -->
 
       <div class="container-fluid" data-aos="fade-up" data-aos-delay="100">
